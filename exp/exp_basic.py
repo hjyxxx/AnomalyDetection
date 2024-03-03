ea066d08725f3cc1fe08c02639c1b1c105e33eef
lab <haoji@163.om>
2024-03-03 17:19:02 +0800

    add exp_base

---
+++ exp/exp_basic.py
@@ -0,0 +1,45 @@
+import os
+
+import torch
+from cprint import cprint
+
+
+class ExpBasic:
+    def __init__(self, args):
+        self.args = args
+        self.model_dict = {
+
+        }
+
+        self.device = self._acquire_device()
+        self.model = self._build_model().to(self.device)
+        self._info()
+
+    def _build_model(self):
+        raise NotImplementedError
+
+    def _acquire_device(self):
+        """
+        设置GPU或者CPU运行
+        :return:
+        """
+        if self.args.use_gpu:
+            os.environ["CUDA_VISIBLE_DEVICES"] = str(self.args.gpu)
+            device = torch.device('cuda:{}'.format(self.args.gpu))
+            cprint.info("Use GPU: {}".format(self.args.gpu))
+        else:
+            device = torch.device('cpu')
+            cprint.info('Use CPU')
+
+        return device
+
+    def _info(self):
+        """
+        打印模型信息
+        :return:
+        """
+        # 计算模型参数个数
+        total_params = sum(p.numel() for p in self.model.parameters())
+        trainable_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+        cprint.info("Total Parameters: {}".format(total_params))
+        cprint.info("Trainable Parameters: {}".format(trainable_params))
